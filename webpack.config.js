a8d620bb6ba367995e7143186d0f1a6930dfaefd
Ivan <stee15454@yandex.ru>
2020-02-11 21:32:39 +0500

    Revert "'Added mini-css-extract-plugin, before setting"
    This reverts commit 2a491bfe7a23d794e0a6aa599872f5d078cfe2f3.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -19,32 +17,17 @@ module.exports = {
   optimization: {
     splitChunks: {chunks: "all"}
   },
-  devServer: {
-
-  },
   plugins: [
       new HTMLWebpackPlugin({
         template: './index.html'
       }),
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      },
-    ]),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {}
-        }, 'css-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
